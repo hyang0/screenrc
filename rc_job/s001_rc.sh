a83e6c7cba86ab9870226f3d9813f5c53e17993b
hyang0 <boo_126@126.com>
2024-07-22 10:50:19 +0800

    screen 兼容性问题
    Signed-off-by: hyang0 <boo_126@126.com>

--- rc_job/s001_rc.sh
+++ rc_job/s001_rc.sh
@@ -6,5 +6,5 @@ cmd="/bin/bash ${RC_DIR}/rc_job/${RC_NAME}.sh"
 SCREEN=/usr/bin/screen
 $SCREEN -dmS $RC_NAME
 $SCREEN -x -S $RC_NAME -p 0 -X stuff "$cmd"
-$SCREEN -x -S $RC_NAME -p 0 -X stuff "\n"
+$SCREEN -x -S $RC_NAME -p 0 -X stuff $'\n'
 
